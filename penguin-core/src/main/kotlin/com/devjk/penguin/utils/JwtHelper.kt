c6ea33b7271ecf612a75f8a5c512973711c6d575
devjk <rmsghk4254@naver.com>
2025-03-30 13:41:13 +0900

    feat(auth): jwt rsa 방식 수정 및 oidc 스펙 추가

--- penguin-core/src/main/kotlin/com/devjk/penguin/utils/JwtHelper.kt
+++ penguin-core/src/main/kotlin/com/devjk/penguin/utils/JwtHelper.kt
@@ -2,21 +2,34 @@ package com.devjk.penguin.utils
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.*
 
 @Component
 class JwtHelper(
-    @Value("\${jwt-key}")
-    private val secretKey: String
+    @Value("\${jwt-private-key}")
+    private val privateKey: String,
+    @Value("\${jwt-public-key}")
+    private val publicKey: String
 ) {
 
     fun create(email: String, role: String, nickname: String): String {
-        val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
+        val key = privateKey
+            .replace("-----BEGIN PRIVATE KEY-----", "")
+            .replace("-----END PRIVATE KEY-----", "")
+            .replace("\\s".toRegex(), "")
+        val keyFactory = KeyFactory.getInstance("RSA")
+        val keySpec = PKCS8EncodedKeySpec(Base64.getDecoder().decode(key))
+        val privateKey = keyFactory.generatePrivate(keySpec) as PrivateKey
+
         val now = Instant.now()
         return Jwts.builder()
             .subject(email)
@@ -24,16 +37,22 @@ class JwtHelper(
             .claim("role", role)
             .issuedAt(Date.from(now))
             .expiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
-            .signWith(key)
+            .signWith(privateKey)
             .compact()
     }
 
     fun getClaimsWithVerify(idToken: String): Claims? {
-        val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
+        val key = publicKey
+            .replace("-----BEGIN PUBLIC KEY-----", "")
+            .replace("-----END PUBLIC KEY-----", "")
+            .replace("\\s".toRegex(), "")
+        val keyFactory = KeyFactory.getInstance("RSA")
+        val keySpec = X509EncodedKeySpec(Base64.getDecoder().decode(key))
+        val publicKey = keyFactory.generatePublic(keySpec) as PublicKey
 
         try {
             val jwt = Jwts.parser()
-                .verifyWith(key)
+                .verifyWith(publicKey)
                 .build()
                 .parseSignedClaims(idToken)
 
