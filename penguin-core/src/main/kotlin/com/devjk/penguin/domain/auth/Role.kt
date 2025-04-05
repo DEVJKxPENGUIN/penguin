97751ec2bb9d7d0194d1610a8a2be989208224ad
devjk <rmsghk4254@naver.com>
2025-04-06 02:04:55 +0900

    feat(web): role 표시 수정

--- penguin-core/src/main/kotlin/com/devjk/penguin/domain/auth/Role.kt
+++ penguin-core/src/main/kotlin/com/devjk/penguin/domain/auth/Role.kt
@@ -1,11 +1,18 @@
 package com.devjk.penguin.domain.auth
 
-enum class Role(private val level: Int) {
-    GUEST(0),
-    NORMAL(10),
-    SUPER(999);
+enum class Role(
+    private val level: Int,
+    private val description: String
+) {
+    GUEST(0, "익명"),
+    NORMAL(10, "유저"),
+    SUPER(999, "관리자");
 
     fun isHigherOrEqualThan(role: Role): Boolean {
         return this.level >= role.level
+    }
+
+    fun description(): String {
+        return this.description
     }
 }
